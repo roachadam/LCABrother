265b53b4667eb9ef909091ababd01a4a75053117
Dawson Jeane <dawsonmjeane@gmail.com>
2019-06-04 20:44:16 -0500

    allow my changes to devdojo to be on git

---
+++ vendor/devdojo/chatter/src/Models/Category.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DevDojo\Chatter\Models;
+
+use App\Organization;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'chatter_categories';
+    protected $guarded = [];
+    public $timestamps = true;
+    public $with = 'parents';
+
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+    public function discussions()
+    {
+        return $this->hasMany(Models::className(Discussion::class),'chatter_category_id');
+    }
+
+    public function parents()
+    {
+        return $this->hasMany(Models::classname(self::class), 'parent_id')->orderBy('order', 'asc');
+    }
+}
