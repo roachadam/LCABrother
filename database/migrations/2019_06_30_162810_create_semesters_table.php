ab19d55a6a65154db23c17fff6fb934910038b51
Dawson Jeane <dawsonmjeane@gmail.com>
2019-06-30 11:38:06 -0500

    Created migration and model

---
+++ database/migrations/2019_06_30_162810_create_semesters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSemestersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('organization_id')->nullable();
+            $table->string('semester_name');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semesters');
+    }
+}
