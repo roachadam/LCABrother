f413ae1c805d4078a5b59ff78eb61bd6234936e6
Dawson Jeane <dawsonmjeane@gmail.com>
2019-06-30 19:46:28 -0500

    Fixes to totals page to reflect changes in semester

--- resources/views/totals/index.blade.php
+++ resources/views/totals/index.blade.php
@@ -2,31 +2,55 @@
 
 @section('content')
     <h3>{{ auth()->user()->organization->getActiveSemester()->semester_name }}</h3>
+
     <div class="card">
-        <div class="card-header">Averages</div>
+        <div class="card-header">{{ __('Organization Goals') }}</div>
         <div class="card-body">
-            Service Hours: {{$averages['service']}}
-            <br>
-            Money Donated: {{$averages['money']}}
-            <br>
-            Involvement Points: {{$averages['points']}}
+            <div class="row">
+                <label  class="col-md-5 col-form-label text-md-right offset-1">Involvement Points Goal:  {{ $goals->points_goal }}<label>
+            </div>
+            <div class="row">
+                <label  class="col-md-5 col-form-label text-md-right offset-1">Study Hours Goal :  {{ $goals->study_goal }}<label>
+            </div>
+            <div class="row">
+                <label  class="col-md-5 col-form-label text-md-right offset-1">Service Hours Goal :  {{ $goals->service_hours_goal }}<label>
+            </div>
+            <div class="row">
+                <label  class="col-md-5 col-form-label text-md-right offset-1 margin-2">Money Donated Goal :  {{ $goals->service_money_goal }}<label>
+            </div>
+            <div class="btn-toolbar">
+                <a href="/goals/edit" class="btn btn-inline btn-primary-outline offset-3">Edit</a>
+                <a href="/goals/{{$goals->id}}/notify" class="btn btn-inline btn-primary-outline" data-toggle="tooltip" data-placement="top" title="Sends a message to members who do not meet a specified threshold">Notify Memebers</a>
+                @if (auth()->user()->isAdmin())
+                    <button type="button" class="btn btn-inline btn-warning-outline" data-toggle="modal" data-target="#modal">New Semester</button>
+                @endif
+            </div>
         </div>
     </div>
 
-    <div class="card">
-        <div class="card-header">Totals</div>
-        <div class="card-body">
-            Service Hours: {{$totals['service']}}  / {{$sumTotals['service']}}
-            <br>
-            Money Donated: {{$totals['money']}}  /  {{$sumTotals['money']}}
-            <br>
-            Involvement Points: {{$totals['points']}} / {{$sumTotals['points']}}
+    <div class="row">
+        <div class="card col-md-5 offset-1 ml-5 mr-5">
+            <div class="card-header">Averages</div>
+            <div class="card-body">
+                Service Hours: {{$averages['service']}}
+                <br>
+                Money Donated: {{$averages['money']}}
+                <br>
+                Involvement Points: {{$averages['points']}}
+            </div>
+        </div>
+
+        <div class="card col-md-5">
+            <div class="card-header">Totals</div>
+            <div class="card-body">
+                Service Hours: {{$totals['service']}}  / {{$sumTotals['service']}}
+                <br>
+                Money Donated: {{$totals['money']}}  /  {{$sumTotals['money']}}
+                <br>
+                Involvement Points: {{$totals['points']}} / {{$sumTotals['points']}}
+            </div>
         </div>
     </div>
-    @if (auth()->user()->isAdmin())
-    <button type="button" class="btn btn-warning-outline" data-toggle="modal" data-target="#modal">New Semester</button>
-
-    @endif
 
 
 
