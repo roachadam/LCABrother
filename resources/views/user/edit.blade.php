845b86ac9a01e71555a32831e9a6b0eb4a0dccf6
Jacob Drury <jacobdrury75@gmail.com>
2019-07-13 21:04:03 -0500

    Change some visuals

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -4,44 +4,38 @@
 <div class="container">
     @include('partials.errors')
     <div class="row ">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     {{ __('Edit Your Details') }}
-                    
+
                 </div>
                 <div class="card-body">
                 <form method="POST" action="/users/update">
                         @csrf
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
+                        <div class="form-group">
+                            <div class="row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+                                <div class="col-md-6">
+                                    <input id="email" type="text" class="form-control" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+                                </div>
                             </div>
 
-                        </div>
+                            <div class="row">
+                                <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
 
-                        <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control" name="phone" value="{{ $user->phone }}" required autocomplete="phone" autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Update Infromation') }}
-                                </button>
+                                <div class="col-md-6">
+                                    <input id="phone" type="text" class="form-control" name="phone" value="{{ $user->phone }}" required autocomplete="phone" autofocus>
+                                </div>
                             </div>
 
-                            <div class="col-md-6 offset-md-4">
-                                <a href="/users/profile" id="cancel" name="cancel" class="btn btn-default">
-                                    Cancel
-                                </a>
+                            {{-- need to center this according to the input boxes on the edit profile page @adam --}}
+                            <div class="row">
+                                <div class="btn-group">
+                                    <button type="submit" class="btn btn-inline btn-primary">{{ __('Update Infromation') }}</button>                                    
+                                    <a href="/users/profile" class="btn btn-inline btn-default" id="cancel" name="cancel">Cancel</a>
+                                </div>
                             </div>
                         </div>
                     </form>
@@ -51,7 +45,7 @@
     </div>
 
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card d-flex">
                 <div class="card-header">{{ __('Edit Your Avatar') }}</div>
                 <div class="card-body align-items-center justify-content-center">
