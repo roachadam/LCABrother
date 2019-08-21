adca0ef96cd184eb67def7ed6bcea5777d7da571
Dawson Jeane <dawsonmjeane@gmail.com>
2019-08-21 13:51:52 -0500

    changes to calendar

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header">{{ $event->name }} Details</div>
                 <div class="card-body">
-                    <p>Date of Event: {{ $event->date_of_event }}</p>
+                    <p>Date of Event: {{ \Carbon\Carbon::parse($event->date_of_event)->format('m/d/Y h:i a') }}</p>
                     <p>Invites per member: {{ $event->num_invites }}</p>
                     <p>Total invites logged: {{ $event->getNumInvites() }}</p>
                 </div>
