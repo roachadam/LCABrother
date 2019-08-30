72fb8695208fe0c2e53ef1133ac3b9962b0bd591
Dawson Jeane <dawsonmjeane@gmail.com>
2019-08-30 14:49:12 -0500

    Fixed buttons on login/register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,11 +42,13 @@
                         <input type="password" name="password_confirmation" id="password_confirmation" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" autocomplete="off">
 
                         <p class="row">
-                                <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }} />
-                                <label for="check">Remember Me</label>
-                              </p>
+                            <input type="checkbox" name="remember" id="remember">
+                            <label for="remember">Remember Me</label>
+                        </p>
                         <button type='submit' class="button button__primary">Register</button>
-						<a href=""><h6 class="left-align" >Forgot your password?</h6></a>
+
+						<a href="{{ route('login') }}"><h6 class="left-align" >Already have an account?</h6></a>
+						<a href="{{ route('password.request') }}"><h6 class="left-align" >Forgot your password?</h6></a>
 					</form>
 				</div>
 			</div>
