6499d14428171029e2950d21f32db5a9704d3c6a
Jacob Drury <jacobdrury75@gmail.com>
2019-09-01 01:42:44 -0500

    Remove create task button from second card

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -51,9 +51,6 @@
         <header class="card-header" style="border-bottom: 0">
             <div class="row">
                 <h2 class="card-title">My Complete Tasks</h2>
-                <div class="ml-auto" id="headerButtons">
-                    <button type="button" class="btn btn-inline btn-primary-outline" data-toggle="modal" data-target="#createTask">Create Task</button>
-                </div>
             </div>
         </header>
 
