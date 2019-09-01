0d30f65d98b631a3118fa2944c1b1e620e897076
Jacob Drury <jacobdrury75@gmail.com>
2019-09-01 18:46:11 -0500

    add phone format on register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        // $data['phone'] = $this->formatPhoneNumber($data['phone']);
+        dd($data['phone'] = $this->formatPhoneNumber($data['phone']));
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -78,8 +78,30 @@ class RegisterController extends Controller
         ]);
     }
 
-    // function formatPhoneNumber($phone)
-    // {
-    //     return preg_replace("/^(\(?\d{3}\)?)?[- .]?(\d{3})[- .]?(\d{4})$/", "(\\1) \\2-\\3", $phone);
-    // }
+    function formatPhoneNumber($phoneNumber)
+    {
+        $phoneNumber = preg_replace('/[^0-9]/', '', $phoneNumber);
+
+        if (strlen($phoneNumber) > 10) {
+            $countryCode = substr($phoneNumber, 0, strlen($phoneNumber) - 10);
+            $areaCode = substr($phoneNumber, -10, 3);
+            $nextThree = substr($phoneNumber, -7, 3);
+            $lastFour = substr($phoneNumber, -4, 4);
+
+            $phoneNumber = '+' . $countryCode . ' (' . $areaCode . ') ' . $nextThree . '-' . $lastFour;
+        } else if (strlen($phoneNumber) == 10) {
+            $areaCode = substr($phoneNumber, 0, 3);
+            $nextThree = substr($phoneNumber, 3, 3);
+            $lastFour = substr($phoneNumber, 6, 4);
+
+            $phoneNumber = '(' . $areaCode . ') ' . $nextThree . '-' . $lastFour;
+        } else if (strlen($phoneNumber) == 7) {
+            $nextThree = substr($phoneNumber, 0, 3);
+            $lastFour = substr($phoneNumber, 3, 4);
+
+            $phoneNumber = $nextThree . '-' . $lastFour;
+        }
+
+        return $phoneNumber;
+    }
 }
