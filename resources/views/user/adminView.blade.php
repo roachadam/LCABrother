4a14dac8f1f85b9cd1818ef04911ca9b8ec77348
Dawson Jeane <dawsonmjeane@gmail.com>
2019-09-06 13:26:03 -0500

    Added ability to pass ownership of the organization

--- resources/views/user/adminView.blade.php
+++ resources/views/user/adminView.blade.php
@@ -21,66 +21,16 @@
 
                     <div class="row justify-content-center">
                         <div class="btn-toolbar">
-                            <button type="button" class="btn btn-inline btn-warning" data-toggle="modal" data-target="#MakeAlumModal">Mark as Alumni</button>
+                            <button type="button" class="btn btn-inline btn-warning-outline" data-toggle="modal" data-target="#MakeAlumModal">Mark as Alumni</button>
+                            @if (auth()->user()->organization->owner->id == auth()->user()->id)
+                            <button type="button" class="btn btn-inline btn-warning-outline" data-toggle="modal" data-target="#ownerPassModal">Pass Ownership</button>
+                            @endif
 
                             @if ($user->role->name !=='admin' && $user->id !== $user->organization->owner->id && auth()->user()->canManageMembers())
                                 <button type="button" class="btn btn-inline btn-danger-outline" data-toggle="modal" data-target="#RemoveFromOrgModal">Remove From Organization</button>
-
-                                <!--.modal for confirming deletion-->
-                                <div class="modal fade" id="RemoveFromOrgModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
-                                                    <i class="font-icon-close-2"></i>
-                                                </button>
-                                                <h4 class="modal-title" id="myModalLabel">Remove Member from Organization</h4>
-                                            </div>
-                                            <form action="/user/{{$user->id}}/organization/remove" method="POST" class="box" >
-                                                <div class="modal-body">
-                                                    @csrf
-                                                    <div class="col-md-12">
-                                                    <p>Are you sure you want to remove {{ $user->name }} from {{ $user->organization->name }}?</p>
-                                                    </div>
-                                                </div>
-
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-inline btn-default" data-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-inline btn-danger">Delete</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div><!--.modal-->
                             @endif
+
                         </div>
-
-                        <!--.modal for confirming move to alumni-->
-                        <div class="modal fade" id="MakeAlumModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
-                                            <i class="font-icon-close-2"></i>
-                                        </button>
-                                        <h4 class="modal-title" id="myModalLabel">Make Alumni</h4>
-                                    </div>
-                                    <form action="/user/{{$user->id}}/alumni" method="POST" class="box" >
-                                        <div class="modal-body">
-                                            @csrf
-                                            <div class="col-md-12">
-                                                <p>Are you sure you want to make {{ $user->name }} an Alumni of {{ $user->organization->name }}?</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-inline btn-default" data-dismiss="modal">Close</button>
-                                            <button type="submit" class="btn btn-inline btn-primary">Yes</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div><!--.modal-->
                     </div>
                 </div>
             </div>
@@ -113,8 +63,88 @@
     </div>
 </div>
 
+<!--.modal for confirming deletion-->
+<div class="modal fade" id="RemoveFromOrgModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
+                    <i class="font-icon-close-2"></i>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">Remove Member from Organization</h4>
+            </div>
+            <form action="/user/{{$user->id}}/organization/remove" method="POST" class="box" >
+                <div class="modal-body">
+                    @csrf
+                    <div class="col-md-12">
+                    <p>Are you sure you want to remove {{ $user->name }} from {{ $user->organization->name }}?</p>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-inline btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-inline btn-danger">Delete</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div><!--.modal-->
+
+    <!--.modal for confirming move to alumni-->
+<div class="modal fade" id="MakeAlumModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
+                    <i class="font-icon-close-2"></i>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">Make Alumni</h4>
+            </div>
+            <form action="/user/{{$user->id}}/alumni" method="POST" class="box" >
+                <div class="modal-body">
+                    @csrf
+                    <div class="col-md-12">
+                        <p>Are you sure you want to make {{ $user->name }} an Alumni of {{ $user->organization->name }}?</p>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-inline btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-inline btn-primary">Yes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div><!--.modal-->
+
+<div class="modal fade" id="ownerPassModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close">
+                    <i class="font-icon-close-2"></i>
+                </button>
+                <h4 class="modal-title" id="myModalLabel">Make Owner</h4>
+            </div>
+            <form action="{{route('organization.passOwner', [auth()->user()->organization,$user])}}" method="POST" class="box" >
+                <div class="modal-body">
+                    @csrf
+                    <div class="col-md-12">
+                        <p>Are you sure you want to make {{ $user->name }} the owner  of {{ $user->organization->name }}?</p>
+                    </div>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-inline btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-inline btn-primary">Yes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div><!--.modal-->
+@endsection
+
 @section('js')
     <script src="js/lib/bootstrap-sweetalert/sweetalert.min.js"></script>
 @endsection
 
-@endsection
