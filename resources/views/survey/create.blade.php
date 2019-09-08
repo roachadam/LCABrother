7c102cb8d738fe69d8838454b0d69ab954ff47ad
Jacob Drury <jacobdrury75@gmail.com>
2019-09-08 12:34:21 -0500

    take out unused stuff

--- resources/views/survey/create.blade.php
+++ resources/views/survey/create.blade.php
@@ -29,27 +29,23 @@
                 <label class="m-t-md">Survey Description</label>
                 <textarea type="text" name="desc" id="desc" class="form-control" required></textarea>
 
+                <div class="row m-t-md">
+                    <input type="text" name="field_name[]" id="field_name[]" placeholder="Survey Field Name" class="form-control col-md-4" required>
 
-                    <div id="inputs">
-                        <div class="row m-t-md">
-                            <input type="text" name="field_name[]" id="field_name[]" placeholder="Survey Field Name" class="form-control col-md-4" required>
-
-                            <select name="field_type[]" id="select-0" class=" form-control col-md-4 offset-1" required>
-                                <option value="text">Text</option>
-                                <option value="date">Date</option>
-                                <option value="textarea">TextArea</option>
-                                <option value="checkbox">CheckBox</option>
-                                <option value="select">Dropdown</option>
-                            </select>
-                        </div>
-                    </div>
+                    <select name="field_type[]" id="select-0" class=" form-control col-md-4 offset-1" required>
+                        <option value="text">Text</option>
+                        <option value="date">Date</option>
+                        <option value="textarea">TextArea</option>
+                        <option value="checkbox">CheckBox</option>
+                        <option value="select">Dropdown</option>
+                    </select>
+                </div>
 
 
-                    <div class="row m-t-md">
-
-                        <div id="frmAccess" ></div>
-                        <div id="frmAccess2" class="offset-1" ></div>
-                    </div>
+                <div class="row">
+                    <div id="frmAccess" ></div>
+                    <div id="frmAccess2" class="offset-1" ></div>
+                </div>
 
                 <div class="row m-t-md">
                     <button id="btn" type="button" class="btn">Add Field</button>
@@ -76,7 +72,7 @@
             counter++;
             let input = document.createElement("input");
             input.id = 'input-' + counter;
-            input.className="form-control m-t-md offset-1";
+            input.className="form-control m-t-md";
             input.type = 'text';
             input.name = 'field_name[]';
             input.placeholder = 'Survey Field Name';
@@ -85,7 +81,7 @@
             const select = document.createElement("select");
             select.id = 'select-' + counter;
             select.name = 'field_type[]';
-            select.className="form-control m-t-md offset-1";
+            select.className="form-control m-t-md";
             select.options[select.options.length] = new Option('Text', 'text');
             select.options[select.options.length] = new Option('Date', 'date');
             select.options[select.options.length] = new Option('TextArea', 'textarea');
