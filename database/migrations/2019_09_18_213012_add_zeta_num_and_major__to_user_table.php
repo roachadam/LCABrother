f55b8150eb4b957440d7c32607530fcf491dd9f8
Jacob Drury <jacobdrury75@gmail.com>
2019-09-18 16:35:06 -0500

    add the delete feature to users migration

--- database/migrations/2019_09_18_213012_add_zeta_num_and_major__to_user_table.php
+++ database/migrations/2019_09_18_213012_add_zeta_num_and_major__to_user_table.php
@@ -27,7 +27,7 @@ class AddZetaNumAndMajorToUserTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            Schema::dropIfExists('users');
         });
     }
 }
